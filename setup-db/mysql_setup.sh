a637ff3154253f82d754cb7f7d56831f91b90d5c
Lopes30787 <72647071+Lopes30787@users.noreply.github.com>
2022-12-04 15:21:38 +0000

    Table Creation

--- setup-db/mysql_setup.sh
+++ setup-db/mysql_setup.sh
@@ -13,6 +13,12 @@ GRANT ALL PRIVILEGES ON * . * TO 'sirs'@'localhost';
 mysql -u sirs -p
 CREATE DATABASE thecork;
 
+USE thecork;
+
+CREATE TABLE restaurant(name VARCHAR(50) NOT NULL, PRIMARY KEY(name));
+CREATE TABLE client(username VARCHAR(50) NOT NULL, pass_hash VARCHAR(50) NOT NULL, PRIMARY KEY(username));
+CREATE TABLE reservation(clientUsername VARCHAR(50) NOT NULL, restaurantName VARCHAR(50) NOT NULL, time DATETIME NOT NULL, nPeople VARCHAR(50) NOT NULL, PRIMARY KEY(clientUsername,restaurantName,time), FOREIGN KEY (clientUsername) REFERENCES client(username), FOREIGN KEY (restaurantName) REFERENCES restaurant(name));
+CREATE TABLE staff(username VARCHAR(50) NOT NULL, restaurantName VARCHAR(50) NOT NULL, pass_hash VARCHAR(50) NOT NULL, PRIMARY KEY(username), FOREIGN KEY(restaurantName) REFERENCES restaurant(name));
 
 # other important commands
 #  -Delete a Database
