f3c29b422020f5cc3adad44ece86fdad4f7ca7c1
Eduardo C <eduardo.a.claudino@gmail.com>
2022-12-06 10:25:08 +0000

    Fixed mysql setup

--- setup-db/2_mysql_setup.sh
+++ setup-db/2_mysql_setup.sh
@@ -4,6 +4,7 @@ sudo apt install mysql-server
 
 #create privileged user
 sudo mysql -u root -e "CREATE USER 'sirs'@'localhost' IDENTIFIED BY 'sirs'; GRANT ALL PRIVILEGES ON * . * TO 'sirs'@'localhost';"
+sudo mysql -u root -e "CREATE USER 'sirs'@'10.0.0.2' IDENTIFIED BY 'sirs'; GRANT ALL PRIVILEGES ON * . * TO 'sirs'@'10.0.0.2';"
 
 echo "Use your preferred text editor to open the mysqld.cnf file. (/etc/mysql/mysql.conf.d/mysqld.cnf)
    Scroll down to the *bind-address* line and change the IP address to 0.0.0.0 .
