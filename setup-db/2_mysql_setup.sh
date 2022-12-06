d463e399c9914af486a6188a38eeae2387e90777
Eduardo C <eduardo.a.claudino@gmail.com>
2022-12-06 00:45:40 +0000

    Improved slightly db setup scripts

--- setup-db/2_mysql_setup.sh
+++ setup-db/2_mysql_setup.sh
@@ -1,6 +1,7 @@
 #install mysql
 sudo apt install mysql-server
 
+
 #create privileged user
 sudo mysql -u root
 CREATE USER 'sirs'@'localhost' IDENTIFIED BY 'sirs';
@@ -9,20 +10,8 @@ GRANT ALL PRIVILEGES ON * . * TO 'sirs'@'localhost';
 #leave mysql
 \q
 
-#create database
-mysql -u sirs -p
-CREATE DATABASE thecork;
+echo "Use your preferred text editor to open the mysqld.cnf file. (/etc/mysql/mysql.conf.d/mysqld.cnf)
+   Scroll down to the *bind-address* line and change the IP address to 0.0.0.0 .
+   Apply the changes made to the MySQL config file by restarting the MySQL service\n"
 
-USE thecork;
-
-CREATE TABLE restaurant(name VARCHAR(50) NOT NULL, PRIMARY KEY(name));
-CREATE TABLE client(username VARCHAR(50) NOT NULL, pass_hash VARCHAR(50) NOT NULL, PRIMARY KEY(username));
-CREATE TABLE reservation(clientUsername VARCHAR(50) NOT NULL, restaurantName VARCHAR(50) NOT NULL, time DATETIME NOT NULL, nPeople VARCHAR(50) NOT NULL, PRIMARY KEY(clientUsername,restaurantName,time), FOREIGN KEY (clientUsername) REFERENCES client(username), FOREIGN KEY (restaurantName) REFERENCES restaurant(name));
-CREATE TABLE staff(username VARCHAR(50) NOT NULL, restaurantName VARCHAR(50) NOT NULL, pass_hash VARCHAR(50) NOT NULL, PRIMARY KEY(username), FOREIGN KEY(restaurantName) REFERENCES restaurant(name));
-
-# other important commands
-#  -Delete a Database
-#  DROP DATABASE dbname;
-#
-#  -Delete a User
-#  DROP USER 'thecork'@'localhost';
+echo "Then run `sudo systemctl restart mysql`"
