fc3bb276460172b2d0bd8417c14415315f77c081
Tomas Pereira <tomas.maria.pereira@hotmail.com>
2023-01-01 19:11:55 -0500

    Slight populate tweaks

--- setup-db/3_mysql_populate.sh
+++ setup-db/3_mysql_populate.sh
@@ -2,12 +2,12 @@
 mysql -u sirs -psirs -e "CREATE DATABASE thecork;"
 
 # table creation
-mysql -u sirs -psirs thecork -e "CREATE TABLE restaurant(name VARCHAR(50) NOT NULL, PRIMARY KEY(name));"
-mysql -u sirs -psirs thecork -e "CREATE TABLE client(username VARCHAR(50) NOT NULL, pass_hash CHAR(64) NOT NULL, pass_salt CHAR(10) NOT NULL, wallet INT NOT NULL, PRIMARY KEY(username));"
-mysql -u sirs -psirs thecork -e "CREATE TABLE reservation(clientUsername VARCHAR(50) NOT NULL, restaurantName VARCHAR(50) NOT NULL, time DATETIME NOT NULL, nPeople VARCHAR(50) NOT NULL, PRIMARY KEY(clientUsername,restaurantName,time), FOREIGN KEY (clientUsername) REFERENCES client(username), FOREIGN KEY (restaurantName) REFERENCES restaurant(name));"
-mysql -u sirs -psirs thecork -e "CREATE TABLE staff(username VARCHAR(50) NOT NULL, restaurantName VARCHAR(50) NOT NULL, pass_hash CHAR(64) NOT NULL, pass_salt CHAR(10) NOT NULL, PRIMARY KEY(username), FOREIGN KEY(restaurantName) REFERENCES restaurant(name));"
-mysql -u sirs -psirs thecork -e "CREATE TABLE giftcards(id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL, nonce INT(16) NOT NULL, owner VARCHAR(50) NOT NULL, PRIMARY KEY(id,nonce), FOREIGN KEY (owner) REFERENCES client(username));"
-mysql -u sirs -psirs thecork -e "CREATE TABLE auth_tokens(id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL, nonce INT(16) NOT NULL, username_client VARCHAR(50), username_staff VARCHAR(50), expiration_date DATETIME NOT NULL, PRIMARY KEY(id,nonce), FOREIGN KEY (username_client) REFERENCES client(username),FOREIGN KEY (username_staff) REFERENCES staff(username));"
+mysql -u sirs -psirs thecork -e "CREATE TABLE restaurant(name VARCHAR(32) NOT NULL, PRIMARY KEY(name));"
+mysql -u sirs -psirs thecork -e "CREATE TABLE client(username VARCHAR(32) NOT NULL, pass_hash CHAR(64) NOT NULL, pass_salt CHAR(10) NOT NULL, wallet INT NOT NULL, PRIMARY KEY(username));"
+mysql -u sirs -psirs thecork -e "CREATE TABLE reservation(clientUsername VARCHAR(32) NOT NULL, restaurantName VARCHAR(32) NOT NULL, time DATETIME NOT NULL, nPeople INT NOT NULL, PRIMARY KEY(clientUsername,restaurantName,time), FOREIGN KEY (clientUsername) REFERENCES client(username), FOREIGN KEY (restaurantName) REFERENCES restaurant(name));"
+mysql -u sirs -psirs thecork -e "CREATE TABLE staff(username VARCHAR(32) NOT NULL, restaurantName VARCHAR(32) NOT NULL, pass_hash CHAR(64) NOT NULL, pass_salt CHAR(10) NOT NULL, PRIMARY KEY(username), FOREIGN KEY(restaurantName) REFERENCES restaurant(name));"
+mysql -u sirs -psirs thecork -e "CREATE TABLE giftcards(id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL, nonce INT(16) NOT NULL, owner VARCHAR(32) NOT NULL, PRIMARY KEY(id,nonce), FOREIGN KEY (owner) REFERENCES client(username));"
+mysql -u sirs -psirs thecork -e "CREATE TABLE auth_tokens(id INT UNSIGNED UNIQUE AUTO_INCREMENT NOT NULL, nonce INT(16) NOT NULL, username_client VARCHAR(32), username_staff VARCHAR(32), expiration_date DATETIME NOT NULL, PRIMARY KEY(id,nonce), FOREIGN KEY (username_client) REFERENCES client(username),FOREIGN KEY (username_staff) REFERENCES staff(username));"
 
 #
 # populate tables
